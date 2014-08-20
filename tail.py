3fbd9a425e304a0a3d244915b288f364c54f9f28
thanhnt <thanhnt@vccloud.vn>
2014-08-20 18:15:20 +0700

    sleep 1 second after once readfile time

--- tail.py
+++ tail.py
@@ -1,3 +1,5 @@
+import time
+
 def tail_F(filename):
     p = 0
     with open(filename, 'r') as f:
@@ -8,6 +10,8 @@ def tail_F(filename):
             if new_line:
                 print new_line
                 print str(p).center(10).center(80, '=')
+            # while True is disaster, see you top on how this use CPU.
+            time.sleep(1)
 
 if __name__ == '__main__':
     filename = 'whileloop.py'
