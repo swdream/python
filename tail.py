06d34c53a208e92faea67435879c83b4527e920d
thanhnt <thanhnt@vccloud.vn>
2014-08-20 16:52:44 +0700

    simulate tail -f command line

---
+++ tail.py
@@ -0,0 +1,14 @@
+def tail_F(filename):
+    p = 0
+    with open(filename, 'r') as f:
+        while True:
+            f.seek(p)
+            new_line = f.read()
+            p = f.tell()
+            if new_line:
+                print new_line
+                print str(p).center(10).center(80, '=')
+
+if __name__ == '__main__':
+    filename = 'whileloop.py'
+    tail_F(filename)
