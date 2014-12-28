201cdc893f374a869a16b9e009c9732a336bffdb
thanhnt <thanhnt@vccloud.vn>
2014-12-28 21:43:02 +0700

    refactor code

--- sleep_sort.py
+++ sleep_sort.py
@@ -11,18 +11,9 @@ import multiprocessing as mtp
 def sleep_and_print(num):
     """ Sleep num seconds, then print that number
     """
-    time.sleep(int(num))
-    print "%d " % int(num),
+    time.sleep(num)
+    print "%d " % num,
 
-
-def value_is_int(value):
-    """ Check a value is a int or not
-    """
-    try:
-        intVal = int(value)
-        return True
-    except:
-        return False
 
 if  __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -31,10 +22,9 @@ if  __name__ == "__main__":
         sys.exit(1)
 
     nums = sys.argv[1:]
-    for num in nums:
-        if not value_is_int(num):
-            print "ERROR, all arguments must be the integer"
-            sys.exit(1)
-        else:
-            p = mtp.Process(target=sleep_and_print, args=(num, ))
+    try:
+        for num in nums:
+            p = mtp.Process(target=sleep_and_print, args=(int(num), ))
             p.start()
+    except Exception as e:
+        print "ERROR, %s" % e
